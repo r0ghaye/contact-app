2803e550dfb26cfbf6ad2c3d909a19072fe91c64
Roghaye <roghayesafari1997@gmail.com>
2024-03-29 19:59:34 +0330

    groupList component added

--- src/component/CreateGroup.jsx
+++ src/component/CreateGroup.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
+import GroupList from './GroupList'
 
 function CreateGroup() {
     const [nameGroup, setNameGroup] = useState("")
+    const [nameGroups, setNameGroups] = useState([])
 
     const changeHandler = (event) => {
         setNameGroup(event.target.value)
@@ -11,9 +13,13 @@ function CreateGroup() {
 
   return (
     <div>
+        <div>
         <h2>Create a group</h2>
         <input type='text' placeholder='The name of the group' value={nameGroup} onChange={changeHandler}/>
         <button >Add Group</button>
+        </div>
+
+        <GroupList groups={nameGroups}/>
     </div>
   )
 }
---
+++ src/component/GroupList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GroupList() {
+  return (
+    <div>
+        <h1>Group list</h1>
+        <p></p>
+    </div>
+  )
+}
+
+export default GroupList
